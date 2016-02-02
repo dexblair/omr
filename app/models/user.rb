1b14695cc3e3ed084c0cefeceb98fa8d92a9701d
Dexter Blair <dexter.blair@gmail.com>
2016-02-02 02:54:35 -0500

    Added user associations to pins.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :pins
+
   #vaildates	:password,	:presence => true,
   #											:confirmation => true,
   #											:length => {:within => 6..40},
