5160f7dacdc12deae4c78b15be5ae1fdcd798436
Dexter Blair <dexter.blair@gmail.com>
2016-02-01 23:04:10 -0500

    Devise functional.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  #vaildates	:password,	:presence => true,
+  #											:confirmation => true,
+  #											:length => {:within => 6..40},
+  #											:on => :create,
+  #											:if => :password,
+  #											:format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\z/}
+  
 end
